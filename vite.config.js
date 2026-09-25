12e3f7f4f570c4db2655c5564d709f2e2caf5d21
Sowmiyasrini <sowmiyasrinivasan912@gmail.com>
2026-09-25 11:21:39 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// Thilip
+//import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/2026_ecommerce_project/",
+// });
+
+// Sowmi
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/2026_ecommerce_project/",
+  base: "/demomaligai/",
 });
